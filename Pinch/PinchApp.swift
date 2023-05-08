c7b0b82791c389b1d9aca05d3cc50fda3d223b4f
Jasmine <jasminehweying@gmail.com>
2023-05-08 17:14:31 +0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jasmine Lai Hweeying on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
